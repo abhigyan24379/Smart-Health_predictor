66b6cd8f8b9df93a03054cb9807affd4bb09ae25
abhigyan24379 <abhigyan03062003@gmail.com>
2025-07-13 14:35:01 +0530

    as the model was not working properly it took me two day to find what i was going wrong, I scaled the model and then i didnt used it for no rsn and i think it slipped of my mind

--- app/app.py
+++ app/app.py
@@ -2,10 +2,16 @@ import streamlit as st
 import pandas as pd 
 import numpy as np
 import joblib
+import shap 
+import matplotlib.pyplot as plt
 import os
+
+
 base_path = os.path.dirname(__file__)
 model_path = os.path.join(base_path, "../models/rf_model.pkl")
 model = joblib.load(model_path)
+scaler_path = os.path.join(base_path, "../models/scaler.pkl")
+scaler = joblib.load(scaler_path)
 st.set_page_config(page_title="Smart Health Prediction", layout="centered")
 
 st.title("Smart Health Predictor")
@@ -36,11 +42,31 @@ if st.button("Predict"):
         }
     )
     # Predict 
-    prediction = model.predict(input_data)[0]
+    scaler_input = scaler.transform(input_data)
+    prediction = model.predict(scaler_input)[0]
     result = "Not Diabetic" if prediction == 0 else "Diabetic"
     
     st.subheader("Prediction Result")
     st.success(result)
+    
+    explainer = shap.Explainer(model, input_data)
+    shap_value = explainer(input_data)
+    
+    # ploting the individual prediction waterfall 
+    st.subheader("Prediction Explanation")
+    st.write("Feature contributions to the prediction:")
+    
+    fig , ax = plt.subplots()
+    shap.plots.waterfall(shap_value[0,:, 1], show=False)
+    st.pyplot(fig)
+    
+    x_test = joblib.load(os.path.join(base_path, "../models/x_test.pkl"))
+    explainer = shap.Explainer(model, x_test)
+    shap_values = explainer(x_test, check_additivity=False)
+    
+    st.subheader("Global Feature Importance")
+    shap.plots.beeswarm(shap_values[..., 1], show=False)
+    st.pyplot(plt.gcf())
     
     
 
